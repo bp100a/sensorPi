616c42b33f00b216a0642efb9b9fa4257b4541e8
Harry <bp100a@hotmail.com>
2017-02-18 02:32:13 +0000

    added nest.py and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sensorPi',
+      version='1.0',
+      description='Temperature Sensor',
+      author='Harry Collins',
+      author_email='bp100a@hotmail.com',
+      url='http://www.altaitech.com/',
+      scripts=['monitor.py'],
+     )
